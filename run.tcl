40564c3daf74386ddd567645b1351615de5c9946
Ufko <ufko.org@proton.me>
2025-02-28 10:38:53 +0100

    comment

--- run.tcl
+++ run.tcl
@@ -1,3 +1,4 @@
+# ---------------------------------------------------------------------
 # Copyright (c) 2025, Ufko.org
 # Licensed under the Apache License, Version 2.0. 
 # See LICENSE file in the project root for details.
